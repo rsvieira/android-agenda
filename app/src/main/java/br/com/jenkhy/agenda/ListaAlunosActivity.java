c5d2ef684da48e2671915bf051079b06637b49d1
Jenkhy <ramonsv89@gmail.com>
2017-12-19 01:06:07 -0200

    Aula 3

--- app/src/main/java/br/com/jenkhy/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/jenkhy/agenda/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package br.com.jenkhy.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -19,6 +22,15 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
 
         lista.setAdapter(arrayAdapter);
+
+        Button novoAluno = findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
